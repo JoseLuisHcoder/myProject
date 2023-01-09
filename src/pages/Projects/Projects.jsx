b8d114db640ac37821c959a5c3d18949f0ac1ab0
JoseLuisHcoder <zonajl@gmail.com>
2023-01-09 14:45:08 -0400

    add projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,17 +1,183 @@
 import React from 'react'
 import './Projects.css'
-const Projects = () => {
-  return (
-    <div className='project'>
-    
-      <div className='project__body'>
-          <h1 className='project__title'>JOSE LUIS HUANCA</h1>
-          <h3 className='project__subtitle'>Development Full Stack</h3>
-          <div className='project__box'></div>
+import '../../App.css'
 
-      </div>
-  </div>
-  )
+const Projects = () => {
+    return (
+        <div className='project'>
+
+            <div className='project__hero'>
+                <h1 className='project__title'>SOME PROJECTS</h1>
+                <h3 className='project__subtitle'>That I have been working on</h3>
+                <div className='project__box'></div>
+
+            </div>
+            <div className='project__body'>
+                <div className='project__body__img'>
+                    <div className="shadow_swhow_mini">
+                        <div className="deroul_titre">
+                            <h1>My Portfolio</h1>
+                            <p>Technologies used</p>
+                            <div className='deroul__icons'>
+                                <img src="/images/html-5.svg" alt="" />
+                                <img src="/images/javascript.svg" alt="" />
+                                <img src="/images/css-3.svg" alt="" />
+                                <img src="/images/react.svg" alt="" />
+                            </div>
+                        </div>
+                        <div className="deroul_soustitre">
+                            <div className='project__btn'>
+                                <a href="https://beatiful-ecommerce-9qyxjb4et-joseluishcoder.vercel.app/" target="_blank">
+                                    <button className='btn'><p>View Demo</p></button>
+                                </a>
+                                <a href="https://github.com/JoseLuisHcoder/myProject" target="_blank">
+
+                                    <button className='btn'><p>View Code</p></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='project__body__img'>
+                    <div className="shadow_swhow_mini body__img2">
+                        <div className="deroul_titre">
+                            <h1>Ecommerce</h1>
+                            <p>Technologies used</p>
+                            <div className='deroul__icons'>
+                                <img src="/images/html-5.svg" alt="" />
+                                <img src="/images/javascript.svg" alt="" />
+                                <img src="/images/css-3.svg" alt="" />
+                                
+                            </div>
+                        </div>
+                        <div className="deroul_soustitre">
+                            <div className='project__btn'>
+                                <a href="https://beatiful-ecommerce-9qyxjb4et-joseluishcoder.vercel.app/" target="_blank">
+                                    <button className='btn'><p>View Demo</p></button>
+                                </a>
+                                <a href="https://github.com/JoseLuisHcoder/JLH_Ecommerce" target="_blank">
+
+                                    <button className='btn'><p>View Code</p></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='project__body__img'>
+                    <div className="shadow_swhow_mini body__img3">
+                        <div className="deroul_titre">
+                            <h1>Ecommerce</h1>
+                            <p>Technologies used</p>
+                            <div className='deroul__icons'>
+                                <img src="/images/html-5.svg" alt="" />
+                                <img src="/images/javascript.svg" alt="" />
+                                <img src="/images/css-3.svg" alt="" />
+                                
+                            </div>
+                        </div>
+                        <div className="deroul_soustitre">
+                            <div className='project__btn'>
+                                <a href="https://beatiful-ecommerce-9qyxjb4et-joseluishcoder.vercel.app/" target="_blank">
+                                    <button className='btn'><p>View Demo</p></button>
+                                </a>
+                                <a href="https://github.com/JoseLuisHcoder/JLH_Ecommerce" target="_blank">
+
+                                    <button className='btn'><p>View Code</p></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                
+                <div className='project__body__img'>
+                    <div className="shadow_swhow_mini body__img4">
+                        <div className="deroul_titre">
+                            <h1>My Portfolio</h1>
+                            <p>Technologies used</p>
+                            <div className='deroul__icons'>
+                                <img src="/images/html-5.svg" alt="" />
+                                <img src="/images/javascript.svg" alt="" />
+                                <img src="/images/css-3.svg" alt="" />
+                                <img src="/images/react.svg" alt="" />
+                            </div>
+                        </div>
+                        <div className="deroul_soustitre">
+                            <div className='project__btn'>
+                                <a href="https://beatiful-ecommerce-9qyxjb4et-joseluishcoder.vercel.app/" target="_blank">
+                                    <button className='btn'><p>View Demo</p></button>
+                                </a>
+                                <a href="https://github.com/JoseLuisHcoder/myProject" target="_blank">
+
+                                    <button className='btn'><p>View Code</p></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='project__body__img'>
+                    <div className="shadow_swhow_mini body__img4">
+                        <div className="deroul_titre">
+                            <h1>My Portfolio</h1>
+                            <p>Technologies used</p>
+                            <div className='deroul__icons'>
+                                <img src="/images/html-5.svg" alt="" />
+                                <img src="/images/javascript.svg" alt="" />
+                                <img src="/images/css-3.svg" alt="" />
+                                <img src="/images/react.svg" alt="" />
+                            </div>
+                        </div>
+                        <div className="deroul_soustitre">
+                            <div className='project__btn'>
+                                <a href="https://beatiful-ecommerce-9qyxjb4et-joseluishcoder.vercel.app/" target="_blank">
+                                    <button className='btn'><p>View Demo</p></button>
+                                </a>
+                                <a href="https://github.com/JoseLuisHcoder/myProject" target="_blank">
+
+                                    <button className='btn'><p>View Code</p></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='project__body__img'>
+                    <div className="shadow_swhow_mini body__img6">
+                        <div className="deroul_titre">
+                            <h1>Ecommerce</h1>
+                            <p>Technologies used</p>
+                            <div className='deroul__icons'>
+                                <img src="/images/html-5.svg" alt="" />
+                                <img src="/images/javascript.svg" alt="" />
+                                <img src="/images/css-3.svg" alt="" />
+                                
+                            </div>
+                        </div>
+                        <div className="deroul_soustitre">
+                            <div className='project__btn'>
+                                <a href="https://beatiful-ecommerce-9qyxjb4et-joseluishcoder.vercel.app/" target="_blank">
+                                    <button className='btn'><p>View Demo</p></button>
+                                </a>
+                                <a href="https://github.com/JoseLuisHcoder/JLH_Ecommerce" target="_blank">
+
+                                    <button className='btn'><p>View Code</p></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Projects
