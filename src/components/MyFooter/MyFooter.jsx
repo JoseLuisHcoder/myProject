12a3a6e45c0642cb8fcd8ebe42615b69b067a6db
JoseLuisHcoder <zonajl@gmail.com>
2023-01-10 07:03:51 -0400

    add my footer completed

--- src/components/MyFooter/MyFooter.jsx
+++ src/components/MyFooter/MyFooter.jsx
@@ -4,7 +4,18 @@ import '../../App.css'
 
 const MyFooter = () => {
   return (
-    <div>MyFooter</div>
+    <div className='myfooter'>
+      <div className='myfooter__container'>
+          <p className='myfooter__title'>Jose Luis Huanca @ 2023</p>
+          <div className='myfooter__colors'>
+              <div className='color1'></div>
+              <div className='color2 '></div>
+              <div className='color3'></div>
+              <div className='color4'></div>
+          </div>
+          <div className='myfooter__end'>"Thank you for the inspiration, from Miguel Basanta"</div>
+      </div>
+    </div>
   )
 }
 
