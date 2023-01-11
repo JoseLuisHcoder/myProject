3d745006f77b89e7cdec06fcc1b9ab1174ac72ed
JoseLuisHcoder <zonajl@gmail.com>
2023-01-11 09:43:52 -0400

    update build

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,8 +10,17 @@ const Home = () => {
       <CardQuotes />
         <div className='home__body'>
             <h1 className='home__title'>JOSE LUIS HUANCA</h1>
-            <h3 className='home__subtitle'> Front-end Developer</h3>
+            <div className='console-container'> 
+                <span id='text'></span>
+                <div class='console-underscore' id='console'>&#95;</div>
+            </div>
             <div className='home__box'></div>
+
+            {/* <div class='console-container'>
+              <span id='text'></span>
+              <div class='console-underscore' id='console'>&#95;</div>
+            </div> */}
+
 
         </div>
         
